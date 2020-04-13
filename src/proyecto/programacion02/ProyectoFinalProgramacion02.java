0045d5d150a2f44606a3c842503241fff1d03ee6
Reymond <reymond1702@gmail.com>
2020-04-13 09:35:54 -0600

    Creacion del proyecto

---
+++ src/proyecto/programacion02/ProyectoFinalProgramacion02.java
@@ -0,0 +1,17 @@
+
+package proyecto.programacion02;
+
+/**
+ *
+ * @author Reymond
+ */
+public class ProyectoFinalProgramacion02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
